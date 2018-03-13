204fe020ffeb5ef6cb9417e31c35b19ce95b7aa5
Robin Sollfors <a17robso@hs.local>
2018-03-13 13:04:21 +0100

    Ring of destruction and layers

---
+++ Assets/Scripts/RingOfDestruction.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RingOfDestruction : MonoBehaviour {
+
+    private ParticleSystem parSys;
+
+    List<ParticleSystem.Particle> enterList = new List<ParticleSystem.Particle>();
+
+    // Use this for initialization
+    void Start () {
+        parSys = GetComponent<ParticleSystem>();
+	}
+
+    //void OnParticleTrigger()
+    //{
+    //    int numEnter = parSys.GetTriggerParticles(ParticleSystemTriggerEventType.Enter, enterList);
+    //    Debug.Log(numEnter);
+
+    //    for (int i = 0; i < numEnter; i++)
+    //    {
+    //        Debug.Log("HIT");
+    //    }
+    //}
+
+    void OnParticleCollision(GameObject other)
+    {
+        Destroy(other);
+    }
+}
---
+++ Assets/Scripts/RingOfDestructionHit.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RingOfDestructionHit : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void OnParticleCollision(GameObject TargetedParticle)
+    {
+        Debug.Log("Collided with particle");
+    }
+}
