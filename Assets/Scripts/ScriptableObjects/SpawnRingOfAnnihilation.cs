107af82a8633f8209d328ec1f8864c2e78d66f88
Robin Sollfors <a17robso@hs.local>
2018-03-13 15:53:18 +0100

    Added an action that spawns the ring of annihilation

---
+++ Assets/Scripts/ScriptableObjects/SpawnRingOfAnnihilation.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[CreateAssetMenu(menuName = "Actions/SpawnRingOfAnnihilation")]
+public class SpawnRingOfAnnihilation : StateAction {
+
+    [Tooltip("The blank")]
+    [SerializeField]
+    private GameObject blank;
+
+    private bool triggered = false;
+
+    public override void Act(StateController controller)
+    {
+        if (triggered == false)
+        {
+            TriggedBlank(controller);
+        }
+    }
+
+    private void TriggedBlank(StateController controller)
+    {
+        Instantiate(blank, controller.playerGameObject.transform.position, new Quaternion());
+    }
+
+
+    public override void Reset(StateController controller)
+    {
+        triggered = false;
+    }
+}
