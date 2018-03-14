06d7b1bc6a5c51d355bda0a9c73593c17bf68d0d
Robin Sollfors <a17robso@hs.local>
2018-03-14 14:57:18 +0100

    New actions for the boss (music change, reset health etc)

---
+++ Assets/Scripts/ScriptableObjects/BossHealthResetAction.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Actions/BossHealthReset")]
+public class BossHealthResetAction : StateAction {
+
+    public override void Act(StateController controller)
+    {
+        ResetHealth(controller);
+    }
+
+    private void ResetHealth(StateController controller)
+    {
+        TrudgesHealthBar hpBar = controller.GetComponentInChildren<TrudgesHealthBar>();
+
+        hpBar.ResetBar();
+    }
+
+    public override void Reset(StateController controller)
+    {
+        ResetHealth(controller);
+    }
+}
---
+++ Assets/Scripts/ScriptableObjects/ChangeMusicAction.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeMusicAction : StateAction {
+
+    [Tooltip("The clip that starts playing")]
+    [SerializeField]
+    private AudioClip startclip;
+
+    [Tooltip("The clip that will loop after teh startclip is done")]
+    [SerializeField]
+    private AudioClip loopClip;
+
+    public override void Act(StateController controller)
+    {
+        ChangeMusic();
+    }
+
+    private void ChangeMusic()
+    {
+        GameObject audioS = GameObject.FindGameObjectWithTag("AudioSource");
+
+        BackgroundMusic bm = audioS.GetComponentInChildren<BackgroundMusic>();
+
+        MonoBehaviour monoBehavior = new MonoBehaviour();
+        monoBehavior.StartCoroutine(bm.PlayEngineSound(startclip, loopClip));
+    }
+
+    public override void Reset(StateController controller)
+    {
+        throw new System.NotImplementedException();
+    }
+}
