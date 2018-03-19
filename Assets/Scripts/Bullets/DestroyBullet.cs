7d8fdb4d687272a2e8c303fbc0728af358639e2a
Robin Sollfors <a17robso@hs.local>
2018-03-19 13:10:19 +0100

    Cleaned DestroyBullet Script

--- Assets/Scripts/Bullets/DestroyBullet.cs
+++ Assets/Scripts/Bullets/DestroyBullet.cs
@@ -3,6 +3,9 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class DestroyBullet : MonoBehaviour {
+
+    //When the gameobject with this script on exits the screen so you can't see it
+    //Destroy it
     private void OnBecameInvisible()
     {
         Destroy(this.gameObject);
