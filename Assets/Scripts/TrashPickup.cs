9f3eaf624a538cf211b1e7abb28c18f9635da83c
Robin Sollfors <a17robso@hs.local>
2018-03-19 13:05:19 +0100

    Cleaned TrudgesHealthBar Script

--- Assets/Scripts/TrashPickup.cs
+++ Assets/Scripts/TrashPickup.cs
@@ -78,7 +78,10 @@ public class TrashPickup : MonoBehaviour {
                     trudgesHealthBar.UpdateBar();
                 }
 
+                //Fills the powerup meter
                 pointSystem.FillMeter(fillMeterWith);
+
+                //Destroy pickup
                 Destroy(gameObject);
             }
 
