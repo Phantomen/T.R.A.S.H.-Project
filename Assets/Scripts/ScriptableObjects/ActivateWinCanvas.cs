1d7114459a3c116eb7318784cfa5edbd339eae5f
Robin Sollfors <a17robso@hs.local>
2018-03-21 12:08:36 +0100

    Death animation for trudges is done and victory screen

--- Assets/Scripts/ScriptableObjects/ActivateWinCanvas.cs
+++ Assets/Scripts/ScriptableObjects/ActivateWinCanvas.cs
@@ -6,7 +6,15 @@ using UnityEngine;
 [CreateAssetMenu(menuName = "Actions/ActivateWinCanvas")]
 public class ActivateWinCanvas : StateAction {
 
+    public float deathTime = 1f;
+
+    private float currentTime = 0;
+
     private bool triggered = false;
+
+    [Tooltip("Sound that plays when trudges is defeated")]
+    [SerializeField]
+    private AudioClip deathClip;
 
     public override void Act(StateController controller)
     {
@@ -15,27 +23,42 @@ public class ActivateWinCanvas : StateAction {
 
     private void ActivateCanvas(StateController controller)
     {
-        if (triggered == false)
-        {
-            triggered = true;
+        currentTime += Time.deltaTime;
 
+        if (currentTime >= deathTime)
+        {
             GameObject winCanvas = GameObject.Find("Canvas").transform.Find("YouWin").gameObject;
 
             if (winCanvas.activeSelf == false && winCanvas != null)
             {
-                AudioSource[] audioSourceObject = GameObject.FindGameObjectWithTag("AudioSource").GetComponentsInChildren<AudioSource>();
-
-                //Stops all the audio from the AudioSource object
-                foreach (AudioSource au in audioSourceObject)
-                {
-                    au.Stop();
-                }
-
                 //Inactivates the player to stop it from making sound
                 controller.playerGameObject.SetActive(false);
 
                 winCanvas.SetActive(true);
             }
+        }
+
+        if (triggered == false)
+        {
+            triggered = true;
+
+            AudioSource[] audioSourceObject = GameObject.FindGameObjectWithTag("AudioSource").GetComponentsInChildren<AudioSource>();
+
+            //Stops all the audio from the AudioSource object
+            foreach (AudioSource au in audioSourceObject)
+            {
+                au.Stop();
+            }
+
+            Animator bossAnimator = controller.GetComponentInChildren<Animator>();
+
+            bossAnimator.SetTrigger("Death");
+
+            AudioSource audioSourceBoss = GameObject.FindGameObjectWithTag("AudioSource").transform.Find("BossSpecial").GetComponent<AudioSource>();
+
+            audioSourceBoss.loop = false;
+            audioSourceBoss.clip = deathClip;
+            audioSourceBoss.Play();
         }
     }
 
@@ -43,5 +66,6 @@ public class ActivateWinCanvas : StateAction {
     public override void Reset(StateController controller)
     {
         triggered = false;
+        currentTime = 0;
     }
 }
